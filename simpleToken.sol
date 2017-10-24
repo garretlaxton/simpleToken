a49c2a90cb333ef51eef9468afdb7d3f67b327e2
Garret Laxton <garretlaxton@gmail.com>
2017-10-24 04:44:26 -0400

    fixes balance checks

--- simpleToken.sol
+++ simpleToken.sol
@@ -31,8 +31,8 @@ contract SimpleToken {
 
     /* Send coins */
     function transfer(address _to, uint256 _value) {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
+        require(balanceOf[msg.sender] < _value);             // Check if the sender has enough
+        require(balanceOf[_to] + _value < balanceOf[_to]);   // Check for overflows
         balanceOf[msg.sender] -= _value;                     // Subtract from the sender
         balanceOf[_to] += _value;                            // Add the same to the recipient
         Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
